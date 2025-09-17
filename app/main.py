8247e593dcc46cfdadb823fd87a2d2ece37a709a
satish2026-27 <s01162280@gmail.com>
2025-09-17 22:29:33 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,33 +1,55 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.core.config import settings
-from app.core.logging import setup_logging
-from app.api.routes.health import router as health_router
-from app.api.routes.process import router as process_router
-from app.api.routes.files import router as files_router
+from app.services.processor import run_professor_code
 
-logger = setup_logging(settings.APP_NAME)
+app = FastAPI()
 
-def create_app() -> FastAPI:
-    app = FastAPI(
-        title=settings.APP_NAME,
-        version=settings.APP_VERSION,
-        docs_url=f"{settings.API_PREFIX}/docs",
-        openapi_url=f"{settings.API_PREFIX}/openapi.json",
-    )
+@app.get("/run")
+def run_idp(start_seq: str, target: float = 0.3):
+    """
+    API endpoint to run professor's IDP Designer.
+    Example: /run?start_seq=VLTKTKYT...&target=0.3
+    """
+    return run_professor_code(start_seq, target)
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.CORS_ORIGINS,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
 
-    app.include_router(health_router, prefix=settings.API_PREFIX)
-    app.include_router(process_router, prefix=settings.API_PREFIX)
-    app.include_router(files_router, prefix=settings.API_PREFIX)
 
-    return app
 
-app = create_app()
+
+
+
+
+
+
+
+
+#from fastapi import FastAPI
+#from fastapi.middleware.cors import CORSMiddleware
+#from app.core.config import settings
+#from app.core.logging import setup_logging
+#from app.api.routes.health import router as health_router
+#from app.api.routes.process import router as process_router
+#from app.api.routes.files import router as files_router
+
+#logger = setup_logging(settings.APP_NAME)
+
+#def create_app() -> FastAPI:
+ #   app = FastAPI(
+  #      title=settings.APP_NAME,
+   #     version=settings.APP_VERSION,
+   #     docs_url=f"{settings.API_PREFIX}/docs",
+   #     openapi_url=f"{settings.API_PREFIX}/openapi.json",
+    #)
+
+#       allow_origins=settings.CORS_ORIGINS,
+ #       allow_credentials=True,
+  #      allow_methods=["*"],
+ #       allow_headers=["*"],
+  #  )
+
+   # app.include_router(health_router, prefix=settings.API_PREFIX)
+    #app.include_router(process_router, prefix=settings.API_PREFIX)
+   # app.include_router(files_router, prefix=settings.API_PREFIX)
+
+   # return app
+
+#app = create_app()
